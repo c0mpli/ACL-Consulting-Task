ae2c7c81329d0d4d04d585b0a03eb51e9db18430
Jash Doshi <72159678+c0mpli@users.noreply.github.com>
2023-03-10 01:08:41 +0530

    all done

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post,UseGuards } from '@nestjs/common';
 import { AdminService } from './admin.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('admin')
 export class AdminController {
     constructor(private adminService: AdminService) {}
 
     @Get('/userdata')
+    @UseGuards(AuthGuard())
     getUserData(){
         return this.adminService.getUserData();
     }
--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -3,9 +3,12 @@ import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from '../auth/schemas/user.schema';
+import { AuthModule } from '../auth/auth.module';
+
 
 @Module({
-  imports: [    
+  imports: [  
+    AuthModule,  
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
   ],
   controllers: [AdminController],
